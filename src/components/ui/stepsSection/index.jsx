55c7c1a6f48ec08f36e7b87810f31a556cedaa6b
Juan <juanfelipecardenas4@gmail.com>
2023-09-28 23:12:30 -0400

    Added: instructions

--- src/components/ui/stepsSection/index.jsx
+++ src/components/ui/stepsSection/index.jsx
@@ -7,26 +7,39 @@ import "./styles.scss";
 const StepsSection = () => {
   return (
     <div className="Steps">
-      <span className="Steps__list">
-        <span className="Steps__item">
-          <h3 className="Steps-title">
-            Step 1: <span className="Step-accent">enter a url</span>
+      <div className="Steps__list">
+        <div className="Steps__item">
+          <h3 className="Steps__title">
+            Step 1: <span className="Steps__accent">enter a url</span>
           </h3>
+          <p className="Steps__text">
+            Enter a valid YouTube video URL, it must be shorter than 3 minutes.
+          </p>
           <img src={step1Img} alt="" className="Steps__image" />
-        </span>
-        <span className="Steps__item">
-          <h3 className="Steps-title">
-            Step 2: <span className="Step-accent">Wait for results</span>
+        </div>
+        <div className="Steps__item">
+          <h3 className="Steps__title">
+            Step 2: <span className="Steps__accent">Wait for results</span>
           </h3>
+          <p className="Steps__text">
+            The application will connect with the vid-stats-api, fetch the
+            YouTube video, get a response object with all the details
+          </p>
           <img src={step2Img} alt="" className="Steps__image" />
-        </span>
-        <span className="Steps__item">
-          <h3 className="Steps-title">
-            Step 3: <span className="Step-accent">View report</span>
+        </div>
+        <div className="Steps__item">
+          <h3 className="Steps__title">
+            Step 3: <span className="Steps__accent">View report</span>
           </h3>
+          <p className="Steps__text">
+            A Results page will appear showing the video, it's title, the
+            description, some quick details such as avg. word length, vocabulary
+            richness, longest word, shortest word, common bigrams,and a word
+            frequency distribution table (which may be sorted).
+          </p>
           <img src={step3Img} alt="" className="Steps__image" />
-        </span>
-      </span>
+        </div>
+      </div>
     </div>
   );
 };
